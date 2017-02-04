88fa766cd9fbc92fa5a52f9b745323de6309bd71
master <core.safecrlf>
2017-02-04 06:11:41 +0500

    Конец пары 04.02.2017
    Начало создание чтения графа из файла реализовано открытие и ошибка при
    открытии.

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -1,4 +1,7 @@
 #include "Graph.h"
+#include <fstream>
+#include <iostream>
+
 
 Graph::Graph() // Описание создания пустого графа
 {
@@ -8,3 +11,32 @@ Graph::Graph() // Описание создания пустого графа
 	N = 0;				// У Графа нет вершин 
 	M = 0;				// У Графа нет ребер
 }
+
+void readGraph(string fileName) 
+{
+	// Создаю объект типа файла и попытаться открыть файл с именем "fileName"
+	ifstream file(fileName.c_str());
+
+	// Если не удалось открыть файл
+	if (!file.is_open())
+	{
+		cout << "Файл невозможно открыть!" << fileName << "\n";
+		return;
+	}
+}
+
+void addEdge(int from, int to, int weight) {}
+
+void removeEdge(int from, int to) {}
+
+int changeEdge(int from, int to, int newWeight) {}
+
+void transformToAdjList() {}
+
+void transformToAdjMatrix() {}
+
+void transformToListOfEdges() {}
+
+void writeGraph(string fileName) {}
+
+bool is_weighted() {}
